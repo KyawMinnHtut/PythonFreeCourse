4ba84caa7a07dfc7ede50915d6ba865c313bc9b0
Kyaw Min Htut <kyawminhtut.monywa@gmail.com>
2022-09-13 01:05:40 +0630

    Day 22 added

--- test.py
+++ test.py
@@ -1,15 +1,33 @@
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    def set_age(self, age):     #verity invalid data
-        if 0 < age < 100:
-            self.age = age
-    def get_age(self):
-        return self.age
-    def display(self):
-        print("Name ", self.name, ", Age ", self.age)
-stu = Student("Kyaw Min Htut", 30)
-print(stu.get_age())
-stu.set_age(11)
-stu.display()
+# Solving producer-consumer problem by Event object
+from multiprocessing import Event
+from random import randint
+from threading import Thread
+from time import sleep
+
+
+items = []
+event = Event()
+class Producer(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+    def run(self):
+        for i in range(10):
+            item = randint(0, 256)
+            items.append(item)
+            print("Produce ", item)
+            sleep(1)
+            event.set() #produceတာကို eventထဲကို ထည့်လိုက်
+            event.clear()   #produceမရရင် eventထဲမှာ ဘာမှ မရှိဘူးလို့ signalလုပ်
+class Consumer(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+    def run(self):
+        for i in range(10):
+            event.wait()    #eventထဲမှာ produceမရှိရင် စောင့်
+            item = items.pop()
+            print("Item consume ", item)
+
+producer = Producer()
+consumer = Consumer()
+producer.start()
+consumer.start()
